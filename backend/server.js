75460ab061fc74aef170809946af32b14fd4ed6e
Bibek Rai <bibekrai655@gmail.com>
2025-06-29 16:29:04 +0545

    controllers created

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,27 @@
 import express from "express"; 
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+import mongoose from "mongoose";
+import productRoutes  from "./routes/product.route.js";
 
 dotenv.config();
 
 
 const app = express();
 
-app.post("/products",async(req,res) => {
-    const product = req.body;
+const PORT = process.env.PORT;
 
-    if (!product.name || !product.price || !product.image){
-        return res.status(400).json({success : false , message : "Please provide all fields"});
-    }
+app.use(express.json());
 
-    const newProduct = new Product(product);
+app.use("/api/products",productRoutes);
 
-    try{
-        await newProduct.save();
-        res.status(201).json({success : true,data : newProduct});
-    }catch(error){
-        console.error("Error in Create Products:", error.messages);
-        res.status(500).json({success : false , message : "Server Error"});
-    }
-});
 
 console.log(process.env.MONGO_URI);
 
 app.listen(5000,() => {
     connectDB();
-    console.log("Server started at http://localhost:5000 ");
+    console.log("Server started at http://localhost:" + PORT);
 });
 
 

155619ae0130fba5d616d58d55776cb0621de84e
Bibek Rai <bibekrai655@gmail.com>
2025-07-07 13:01:34 +0545

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"; 
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
 import mongoose from "mongoose";
@@ -12,10 +13,20 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 
 app.use("/api/products",productRoutes);
 
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    })
+}
 
 console.log(process.env.MONGO_URI);
 
